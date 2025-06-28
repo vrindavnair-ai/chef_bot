3d436c902b7e018e96f538a61dda706ba7f0e868
Vrinda V Nair <vrindavnair@Vrindas-MacBook-Pro.local>
2025-06-28 16:53:30 +0400

    Updated app to use Gemini API and modified requirements

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import openai
+#import openai
 import streamlit as st
+import google.generativeai as genai
 
 #Auth
-openai.api_key = st.secrets(["api_key"])
+#openai.api_key = st.secrets(["api_key"])
+genai.configure(api_key=st.secrets["gemini_api_key"])
 
 #Title
 st.title("Chef Bot")
@@ -14,15 +16,22 @@ Instructions = st.text_area(
 
 if len(Instructions) <100:
     if st.button("Show Options"):
-        response = openai.completions.create(
+        """response = openai.completions.create(
             model = "text-davinchi-003",
             prompt = "Act like my personal chef and give me delicious food suggestion based on my following prompt: "+ Instructions,
             temperature =0,
             max_tokens = 100,
             stop = ["\n"]
         )
-        output = response.choices[0].text
-        st.info(output)
+        output = response.choices[0].text"""
+        model = genai.GenerativeModel("gemini-1.5-pro")
+        prompt = f"Act like my personal chef and give me delicious food suggestion based on the following prompt: {Instructions}"
+        response = model.generate_content(prompt,
+                generation_config=genai.types.GenerationConfig(
+                temperature=0.7,
+                max_output_tokens=100
+            ))
+        st.info(response)
 
 else:
     st.warning(
